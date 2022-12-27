40d4cac5f3f5b0dcb640a1b66f19423b07e93a2c
AsyncMatrix <Catbob614@gmail.com>
2022-12-27 13:15:24 +0000

    ✨ Implementation of new datalink 'Build' project

---
+++ DatalinkTestEZ.server.lua
@@ -0,0 +1,7 @@
+local ServerStorage = game:GetService("ServerStorage")
+local TestEZ = require(script.TestEZ)
+
+_G.DATALINK_DEVELOPER_ACCOUNT_ID = 3
+_G.DATALINK_DEVELOPER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiMzhiYzk2ZGYtODQ4Mi00NGFlLTg3MDMtMWNiMGFkZDJiNzk5IiwiaWF0IjoxNjcwMTkzODE3fQ.efzw5tHOZBrTmfJeAjuuhELHI6z2F7GR8w7y8SXB1v8"
+
+TestEZ.TestBootstrap:run({ ServerStorage.DatalinkSDK })
